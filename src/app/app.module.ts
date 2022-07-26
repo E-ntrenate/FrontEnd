a8281146eed1d95c78f6a5cd0b999bca22b16e51
Santiago Rodríguez Vallejo <sarodriguezva@unal.edu.co>
2022-07-26 15:53:03 -0500

    feat: configuración inicial
    - añadido bootstrap y ng material.
    - añadidos componentes y servicios de prueba mediante CLI.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material.module';
+import { ExampleComponent } from './components/example/example.component';
+import { Example2Component } from './components/example2/example2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleComponent,
+    Example2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
